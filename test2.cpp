f35d87601cc63550b3fed77564e638fe06013713
be84n12son25 <be84n12son25@gmail.com>
2018-05-31 06:16:45 +0000

    Last Test.

--- test2.cpp
+++ test2.cpp
@@ -3,10 +3,5 @@ using namespace std;
 
 int main(void){
     cout<<"test 2."<<endl;
-<<<<<<< HEAD
     cout<<"test 2.1"<<endl;
 }
-=======
-    cout<<"test 2."<<endl;
-}
->>>>>>> e83d62b3f067e7421dc5a241db23cb7876d6de31
