c89da76b2b13dd14d5a8e9d89a1548ae21b76d73
be84n12son25 <be84n12son25@gmail.com>
2018-06-26 02:27:05 +0000

    merge coordinate.h

--- include/coordinate.h
+++ include/coordinate.h
@@ -6,14 +6,11 @@
 using namespace std;
 class Coordinate{
     public:
-<<<<<<< HEAD
-	Coordinate()
-	{
-		x=y=-1;
-	}
-=======
+	    /*Coordinate()
+	    {
+	    	x=y=-1;
+	    }*/
         Coordinate():x(0),y(0){}
->>>>>>> 515cd8751b1ca14150445a04a26ff760adb9ab86
         Coordinate(int xpos, int ypos):x(xpos),y(ypos){}
         ~Coordinate(){}
         int getX() const{
@@ -25,13 +22,11 @@ class Coordinate{
         void show(){
             cout<<x<<','<<y<<endl;
         }
-<<<<<<< HEAD
-	bool operator==(const Coordinate& cor) const{
-        if(this->x==cor.x&&this->y==cor.y)
-            return true;
-        return false;
-	}
-=======
+    	bool operator==(const Coordinate& cor) const{
+            if(this->x==cor.x&&this->y==cor.y)
+                return true;
+            return false;
+    	}
         bool operator<(const Coordinate& coord) const{
             if(x<coord.x)
                 return true;
@@ -49,7 +44,6 @@ class Coordinate{
                 return true;
             return false;
         }
->>>>>>> 515cd8751b1ca14150445a04a26ff760adb9ab86
     protected:
         int x;
         int y;
